dce0ab17fb8bd5304015158cfa91d3c1af917f65
MEvanAW <evananindya@mail.ugm.ac.id>
2020-10-30 12:24:55 +0700

    Memperbaiki jarak antara icon dan tulisan urutkan dan filter pada activity_hasil_telusuri.xml

--- app/src/main/java/com/example/laptopku/HasilTelusuriActivity.kt
+++ app/src/main/java/com/example/laptopku/HasilTelusuriActivity.kt
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class HasilTelusuriActivity : AppCompatActivity() {
+    //inisiasi TextView
+    private lateinit var textView: android.widget.TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hasil_telusuri)
+
+        //ini juga inisiasi untuk TextView
+        textView = findViewById(R.id.urutkanTextView)
+        textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_urutkan,0,0,0)
+        textView.compoundDrawablePadding = 32
+        textView = findViewById(R.id.filterTextView)
+        textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_filter,0,0,0)
+        textView.compoundDrawablePadding = 32
     }
 }
