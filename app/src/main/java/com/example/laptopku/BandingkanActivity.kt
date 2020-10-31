b98f5398427b25e325b7407c8bf35740e6eb13f8
MEvanAW <evananindya@mail.ugm.ac.id>
2020-10-31 12:03:46 +0700

    Menambahkan perpindahan activity pada BandingkanActivity.kt Menyeragamkan atribut activity pada AndroidManifest.xml

--- app/src/main/java/com/example/laptopku/BandingkanActivity.kt
+++ app/src/main/java/com/example/laptopku/BandingkanActivity.kt
@@ -2,10 +2,37 @@ package com.example.laptopku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class BandingkanActivity : AppCompatActivity() {
+class BandingkanActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bandingkan)
+
+        //digunakan untuk pindah ke tampilan telusuri
+        val telusuriImageView: android.widget.ImageView = findViewById(R.id.bandingkanFooterTelusuriImageView)
+        telusuriImageView.setOnClickListener(this)
+
+        //digunakan untuk pindah ke tampilan rekomendasi
+        val rekomendasiImageView: android.widget.ImageView = findViewById(R.id.bandingkanFooterRekomendasiImageView)
+        rekomendasiImageView.setOnClickListener(this)
+
+        //digunakan untuk kembali ke tampilan sebelumnya
+        val kembaliImageView: android.widget.ImageView = findViewById(R.id.bandingkanKembaliImageView)
+        kembaliImageView.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.bandingkanFooterTelusuriImageView ->{
+                val moveIntent = android.content.Intent(this@BandingkanActivity, MainActivity::class.java)
+                startActivity(moveIntent)
+            }
+            R.id.bandingkanFooterRekomendasiImageView ->{
+                val moveIntent = android.content.Intent(this@BandingkanActivity, RekomendasiActivity::class.java)
+                startActivity(moveIntent)
+            }
+            R.id.bandingkanKembaliImageView -> finish()
+        }
     }
 }
