0435944257c9ae0af8fb217ae0b8d4937c3f1142
MEvanAW <evananindya@mail.ugm.ac.id>
2020-11-05 12:49:24 +0700

    Menampilkan grid laptop pada Activity Hasil Telusuri

--- app/src/main/java/com/example/laptopku/HasilTelusuriActivity.kt
+++ app/src/main/java/com/example/laptopku/HasilTelusuriActivity.kt
@@ -3,10 +3,22 @@ package com.example.laptopku
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HasilTelusuriActivity : AppCompatActivity(), View.OnClickListener {
     //inisiasi TextView
     private lateinit var textView: android.widget.TextView
+
+    //inisiasi RecyclerView yang akan ditampilkan untuk grid laptop
+    private lateinit var rvTelusuriLaptop: RecyclerView
+
+    //ArrayList untuk grid laptop
+    private val listLaptop: ArrayList<LaptopTerbaru> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +31,13 @@ class HasilTelusuriActivity : AppCompatActivity(), View.OnClickListener {
         textView = findViewById(R.id.filterTextView)
         textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_filter,0,0,0)
         textView.compoundDrawablePadding = 32
+
+        //ini juga inisiasi untuk RecyclerView
+        rvTelusuriLaptop = findViewById(R.id.rv_telusuri_laptop)
+        rvTelusuriLaptop.setHasFixedSize(true)
+
+        //memanggil data yang ada di firebase bar kui dipancal (ditampilke)
+        loadLaptopTerbaru()
 
         //digunakan untuk pindah ke home (main activity)
         val telusuriImageView: android.widget.ImageView = findViewById(R.id.telusuriFooterTelusuriImageView)
@@ -41,6 +60,31 @@ class HasilTelusuriActivity : AppCompatActivity(), View.OnClickListener {
         kembaliImageView.setOnClickListener(this)
     }
 
+    //fungsi untuk mengambil data dari database firestore
+    private fun loadLaptopTerbaru(){
+        // listTerbaru.clear()
+        val db = FirebaseFirestore.getInstance()
+        db.collection("spekLaptop")
+            .orderBy("tanggalRilis", Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener {result ->
+                for (document in result){
+                    listLaptop.add(LaptopTerbaru(document.getString("namaLaptop")!!,
+                        document.getString("hargaLaptop")!!,
+                        document.getString("gambar")!!))
+                }
+                if(listLaptop.isNotEmpty())
+                    showRecyclerList()
+                else
+                    loadLaptopTerbaru()
+            }
+    }
+    //untuk menampilkan RecyclerView Laptop Terbaru
+    private fun showRecyclerList(){
+        rvTelusuriLaptop.layoutManager = GridLayoutManager(this, 2)
+        val listLaptopTerbaruAdapter = ListLaptopTerbaruAdapter(applicationContext, listLaptop)
+        rvTelusuriLaptop.adapter = listLaptopTerbaruAdapter
+    }
     //fungsi untuk pindah ke tampilan rekomendasi dan bandingkan
     override fun onClick(v: View?){
         when(v?.id){
