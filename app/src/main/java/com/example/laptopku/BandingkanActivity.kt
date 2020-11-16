487e4d297331d66c3417b2ad4c9234c3abdda126
MEvanAW <evananindya@mail.ugm.ac.id>
2020-11-16 18:45:45 +0700

    Menampilkan nama laptop yang ingin dibandingkan pada Activity Bandingkan, bila dibuka dari Activity Favorite

--- app/src/main/java/com/example/laptopku/BandingkanActivity.kt
+++ app/src/main/java/com/example/laptopku/BandingkanActivity.kt
@@ -3,11 +3,21 @@ package com.example.laptopku
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_bandingkan.*
 
 class BandingkanActivity : AppCompatActivity(), View.OnClickListener {
+    //inisiasi laptopFavorite
+    private var laptopFavorite: LaptopFavorite? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bandingkan)
+
+        //memindahkan nama laptopFavorite ke EditText cari laptop yang kiri
+        laptopFavorite = intent.getParcelableExtra("laptopFavorite")
+        if(laptopFavorite != null){
+            cariLaptopKiriEditText.setText(laptopFavorite!!.name)
+        }
 
         //digunakan untuk pindah ke tampilan telusuri
         val telusuriImageView: android.widget.ImageView = findViewById(R.id.bandingkanFooterTelusuriImageView)
