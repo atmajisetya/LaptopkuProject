2e915f5a18a8a1d322b7c1da459adfcfb23755e6
M. Evan Anindya W <evananindya@mail.ugm.ac.id>
2020-11-19 14:16:20 +0700

    Progress back-end Activity Rekomendasi (#10)
    * Menerapkan ellipsize pada item_laptop_favorite.xml
    
    * Mengubah OnClickListener dari foto laptop menjadi seluruh LinearLayout pada adapter item_kolom_laptop.xml
    
    * Membuat Activity Rekomendasi menyimpan masukan budget pengguna
    
    * Membuat Activity Rekomendasi menyimpan masukan keperluan pengguna

--- app/src/main/java/com/example/laptopku/RekomendasiActivity.kt
+++ app/src/main/java/com/example/laptopku/RekomendasiActivity.kt
@@ -7,10 +7,20 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
 class RekomendasiActivity : AppCompatActivity(), View.OnClickListener {
+    // fragmentManager beserta transaction untuk mengatur pergantian fragment
     private lateinit var fragmentManager: FragmentManager
     private lateinit var transaction: FragmentTransaction
+
+    // Inisiasi variabel untuk mengakses Button Selanjutnya dan Sebelumnya
     private lateinit var selanjutnyaButton: android.widget.LinearLayout
     private lateinit var sebelumnyaButton: android.widget.LinearLayout
+
+    // Inisiasi variabel untuk menyimpan fragment
+    val budgetFragment = BudgetFragment()
+    private lateinit var keperluanFragment: KeperluanFragment
+    private lateinit var prioritasFragment: PrioritasFragment
+
+    // Variabel agar Activity dapat mengenali sedang memuat fragment yang mana
     private var currentFragment = "budget"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +29,7 @@ class RekomendasiActivity : AppCompatActivity(), View.OnClickListener {
 
         fragmentManager = supportFragmentManager
         transaction = fragmentManager.beginTransaction()
-        transaction.add(R.id.rekomendasiFrameLayout, BudgetFragment())
+        transaction.add(R.id.rekomendasiFrameLayout, budgetFragment)
         transaction.commit()
 
         // Digunakan untuk pindah ke tampilan telusuri
@@ -45,6 +55,7 @@ class RekomendasiActivity : AppCompatActivity(), View.OnClickListener {
         kembaliImageView.setOnClickListener(this)
     }
 
+    // Isi semua event klik
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.rekomendasiFooterTelusuriImageView ->{
@@ -61,32 +72,68 @@ class RekomendasiActivity : AppCompatActivity(), View.OnClickListener {
             }
             R.id.selanjutnyaButton ->{
                 transaction = fragmentManager.beginTransaction()
-                if (currentFragment == "budget"){
-                    transaction.replace(R.id.rekomendasiFrameLayout, KeperluanFragment())
-                    transaction.addToBackStack(null)
-                    transaction.commit()
-                    currentFragment = "keperluan"
-                    sebelumnyaButton.setBackgroundResource(R.drawable.bg_button_biru)
-                }
-                else if (currentFragment == "keperluan"){
-                    transaction.replace(R.id.rekomendasiFrameLayout, PrioritasFragment())
-                    transaction.addToBackStack(null)
-                    transaction.commit()
-                    currentFragment = "prioritas"
-                }
-                else if (currentFragment == "prioritas"){
-                    transaction.replace(R.id.rekomendasiFrameLayout, BrandFragment())
-                    transaction.addToBackStack(null)
-                    transaction.commit()
-                    currentFragment = "brand"
-                }
-                else if (currentFragment == "brand"){
-                    transaction.replace(R.id.rekomendasiFrameLayout, HasilFragment())
-                    transaction.addToBackStack(null)
-                    transaction.commit()
-                    sebelumnyaButton.visibility = View.GONE
-                    selanjutnyaButton.visibility = View.GONE
-                    currentFragment = "hasil"
+                when (currentFragment) {
+                    "budget" -> {
+                        if(budgetFragment.minEditText.text.isNullOrBlank() || budgetFragment.maxEditText.text.isNullOrBlank()){
+                            showToast("Budget minimal dan budget maximal tidak boleh kosong.")
+                        }
+                        else if(budgetFragment.min > budgetFragment.max){
+                            showToast("Budget minimal harus lebih kecil dari budget maximal.")
+                        }
+                        else{
+                            keperluanFragment = KeperluanFragment()
+                            transaction.replace(R.id.rekomendasiFrameLayout, keperluanFragment)
+                            transaction.addToBackStack(null)
+                            transaction.commit()
+                            sebelumnyaButton.setBackgroundResource(R.drawable.bg_button_biru)
+                            currentFragment = "keperluan"
+                            // START DEBUGGING
+                            showToast("min: " + budgetFragment.min.toString() + ", max: " + budgetFragment.max.toString())
+                            // END DEBUGGING
+                        }
+                    }
+                    "keperluan" -> {
+                        if (!keperluanFragment.gameBerat && !keperluanFragment.kalkulasiRumit && !keperluanFragment.grafis2D &&
+                                !keperluanFragment.grafis3D && !keperluanFragment.editingVideo && !keperluanFragment.pekerjaanRingan)
+                            showToast("Harus ada minimal satu keperluan yang dipilih.")
+                        else{
+                            prioritasFragment = PrioritasFragment()
+                            transaction.replace(R.id.rekomendasiFrameLayout, prioritasFragment)
+                            transaction.addToBackStack(null)
+                            transaction.commit()
+                            currentFragment = "prioritas"
+                            // START DEBUGGING
+                            var keperluan = "keperluan: "
+                            if (keperluanFragment.gameBerat)
+                                keperluan += "game berat, "
+                            if (keperluanFragment.kalkulasiRumit)
+                                keperluan += "kalkulasi rumit, "
+                            if (keperluanFragment.grafis2D)
+                                keperluan += "grafis 2D, "
+                            if (keperluanFragment.grafis3D)
+                                keperluan += "grafis 3D, "
+                            if (keperluanFragment.editingVideo)
+                                keperluan += "editing video, "
+                            if (keperluanFragment.pekerjaanRingan)
+                                keperluan += "pekerjaan ringan."
+                            showToast(keperluan)
+                            // END DEBUGGING
+                        }
+                    }
+                    "prioritas" -> {
+                        transaction.replace(R.id.rekomendasiFrameLayout, BrandFragment())
+                        transaction.addToBackStack(null)
+                        transaction.commit()
+                        currentFragment = "brand"
+                    }
+                    "brand" -> {
+                        transaction.replace(R.id.rekomendasiFrameLayout, HasilFragment())
+                        transaction.addToBackStack(null)
+                        transaction.commit()
+                        sebelumnyaButton.visibility = View.GONE
+                        selanjutnyaButton.visibility = View.GONE
+                        currentFragment = "hasil"
+                    }
                 }
             }
             R.id.rekomendasiKembaliImageView, R.id.sebelumnyaButton -> {
@@ -94,19 +141,7 @@ class RekomendasiActivity : AppCompatActivity(), View.OnClickListener {
                     finish()
                 else {
                     fragmentManager.popBackStack()
-                    when (currentFragment) {
-                        "keperluan" -> {
-                            currentFragment = "budget"
-                            sebelumnyaButton.setBackgroundResource(R.drawable.bg_button_abu)
-                        }
-                        "prioritas" -> currentFragment = "keperluan"
-                        "brand" -> currentFragment = "prioritas"
-                        "hasil" -> {
-                            sebelumnyaButton.visibility = View.VISIBLE
-                            selanjutnyaButton.visibility = View.VISIBLE
-                            currentFragment = "brand"
-                        }
-                    }
+                    sesuaikanCurrentFragmentKetikaKembali()
                 }
             }
         }
@@ -114,6 +149,18 @@ class RekomendasiActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onBackPressed(){
         super.onBackPressed()
+        sesuaikanCurrentFragmentKetikaKembali()
+    }
+
+    fun showToast(text: String){
+        val toast = android.widget.Toast.makeText(applicationContext,
+            text,
+            android.widget.Toast.LENGTH_LONG)
+        toast.setGravity(android.view.Gravity.BOTTOM,0,130)
+        toast.show()
+    }
+
+    fun sesuaikanCurrentFragmentKetikaKembali(){
         when (currentFragment) {
             "keperluan" -> {
                 currentFragment = "budget"
