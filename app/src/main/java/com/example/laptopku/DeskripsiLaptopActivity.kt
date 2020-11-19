3973f2dc395a9df23ea00ea81c1bf4854454df60
MEvanAW <evananindya@mail.ugm.ac.id>
2020-11-19 07:29:19 +0700

    Perbaikan assignment variabel dan penambahan komentar pada DeskripsiLaptopActivity.kt

--- app/src/main/java/com/example/laptopku/DeskripsiLaptopActivity.kt
+++ app/src/main/java/com/example/laptopku/DeskripsiLaptopActivity.kt
@@ -12,18 +12,17 @@ import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_deskripsi_laptop.*
 
 class DeskripsiLaptopActivity : AppCompatActivity() {
-    // Inisiasi RecyclerView yang akan ditampilkan untuk spesifikasi laptop
-    private lateinit var rvSpekLaptop: RecyclerView
-    // Untuk spesifikasi laptop
-    private val listSpekLaptop: ArrayList<SpekLaptop> = arrayListOf()
+    // Variabel untuk menerima operan data spesifikasi laptop dari Activity sebelumnya
     private var laptopTerbaru : LaptopTerbaru? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deskripsi_laptop)
 
-        laptopTerbaru = intent.getParcelableExtra<LaptopTerbaru>("laptopTerbaru")
+        // Assignment variabel laptopTerbaru
+        laptopTerbaru = intent.getParcelableExtra("laptopTerbaru")
 
+        // Mengisi ImageView dengan foto laptop
         Glide.with(applicationContext)
             .load(laptopTerbaru?.photo)
             .apply(
@@ -31,27 +30,28 @@ class DeskripsiLaptopActivity : AppCompatActivity() {
                     Target.SIZE_ORIGINAL))
             .into(img_laptop)
 
+        // Menampilkan nama dan harga laptop
         val tvName: TextView = findViewById(R.id.tv_namaLaptop)
         val tvHarga: TextView = findViewById(R.id.tv_hargaLaptop)
         tvName.text = laptopTerbaru?.name
         tvHarga.text = laptopTerbaru?.price
 
+        // Menampilkan spesifikasi-spesifikasi tunggal laptop
         tv_acAdapter.text = laptopTerbaru?.acadapter
         tv_audio.text = laptopTerbaru?.audio
-        tv_baterai.text = laptopTerbaru?.audio
+        tv_baterai.text = laptopTerbaru?.baterai
         tv_cpu.text = laptopTerbaru?.cpu
         tv_os.text = laptopTerbaru?.os
         tv_layar.text = laptopTerbaru?.layar
         tv_chipset.text = laptopTerbaru?.chipset
-        //tv_grafis.text = laptopTerbaru?.grafis
         tv_memori.text = laptopTerbaru?.memori
         tv_penyimpanan.text = laptopTerbaru?.penyimpanan
         tv_webcam.text = laptopTerbaru?.webcam
         tv_keyboard.text = laptopTerbaru?.keyboard
-        // Komunikasi
-        tv_dimensi.text = laptopTerbaru?.keyboard
+        tv_dimensi.text = laptopTerbaru?.dimensi
         tv_berat.text = laptopTerbaru?.berat
 
+        // Menampilkan spesifikasi-spesifikasi ganda laptop
         for(i in 0 until laptopTerbaru?.grafis!!.size){
             if(i == 0){
                 tv_grafis.text = laptopTerbaru!!.grafis[i]
@@ -60,7 +60,6 @@ class DeskripsiLaptopActivity : AppCompatActivity() {
                 tv_grafis.append(laptopTerbaru!!.grafis[i])
             }
         }
-
         for(i in 0 until laptopTerbaru?.io!!.size){
             if(i == 0){
                 tv_io.text = laptopTerbaru!!.io[i]
@@ -69,7 +68,6 @@ class DeskripsiLaptopActivity : AppCompatActivity() {
                 tv_io.append(laptopTerbaru!!.io[i])
             }
         }
-
         for(i in 0 until laptopTerbaru?.komunikasi!!.size){
             if(i == 0){
                 tv_komunikasi.text = laptopTerbaru!!.komunikasi[i]
@@ -78,7 +76,5 @@ class DeskripsiLaptopActivity : AppCompatActivity() {
                 tv_komunikasi.append(laptopTerbaru!!.komunikasi[i])
             }
         }
-
     }
-
 }
