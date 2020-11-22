d3b7578bfda33a94b572d497b6323856811dce23
MEvanAW <evananindya@mail.ugm.ac.id>
2020-11-22 21:58:36 +0700

    Membuat Activity Rekomendasi menyimpan masukan prioritas pengguna

--- app/src/main/java/com/example/laptopku/PrioritasFragment.kt
+++ app/src/main/java/com/example/laptopku/PrioritasFragment.kt
@@ -4,14 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import androidx.fragment.app.Fragment
 
 class PrioritasFragment : Fragment() {
+    // Variabel untuk menyimpan preferensi prioritas pengguna
+    internal var isPerforma = false
+    internal var isPortabilitas = false
+
+    // Variabel untuk merujuk ke CheckBoxes
+    private lateinit var performaRadioButton: RadioButton
+    private lateinit var portabilitasRadioButton: RadioButton
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_prioritas, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // Set event klik RadioButton performa
+        performaRadioButton = view.findViewById(R.id.performaRadioButton)
+        performaRadioButton.setOnClickListener{
+            isPerforma = true
+            isPortabilitas = false
+        }
+
+        // Set event klik RadioButton portabilitas
+        portabilitasRadioButton = view.findViewById(R.id.portabilitasRadioButton)
+        portabilitasRadioButton.setOnClickListener{
+            isPerforma = false
+            isPortabilitas = true
+        }
     }
 }
