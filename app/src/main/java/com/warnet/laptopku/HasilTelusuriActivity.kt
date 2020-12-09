c59869ab458b7b44f87e437590aad3318c413cc3
MEvanAW <evananindya@mail.ugm.ac.id>
2020-12-09 14:27:33 +0700

    Menghapus import yang tidak perlu.

--- app/src/main/java/com/warnet/laptopku/HasilTelusuriActivity.kt
+++ app/src/main/java/com/warnet/laptopku/HasilTelusuriActivity.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.fragment.app.FragmentTransaction
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.header_cari_laptop.*
 
 class HasilTelusuriActivity : AppCompatActivity(), View.OnClickListener {
